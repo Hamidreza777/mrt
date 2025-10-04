f62750998e5276355c203db16f012c67f7d7b906
hamidreza soltani <h.soltani8452@gmail.com>
2025-10-04 17:08:43 +0330

    first code

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// alert("salam")
+// confirm("yes or no")
+// prompt("meghdar ro vared kon")
+
+// console.log(false)
+// variable
+
+// var const let
+
+// var mrt=12;
+
+// let mrt=12
+
+// const mrt=12;
+// ES6
+// engine v8
+// scope ---- global
+
+/* let mrt = 18;
+
+{
+  let mrt = 15;
+  console.log(mrt);
+}
+
+console.log(mrt) */
+
+/* function sum(taghi,naghi) {
+  console.log(taghi+naghi)
+}
+
+function divide(x,y,z,w) {
+    console.log(x+y-z/w)
+}
+ */
+/* function mrt(x, y) {
+  return x + y;
+}
+
+
+alert(mrt(15,8)) */
+
+// pure js
+
+/* const mrt = (x,y) => {
+  console.log(x-y);
+};
+
+mrt(8,4); */
